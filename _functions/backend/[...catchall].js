c751b653bc3ef2013048f2ec36e300732cb12168
clinilab <clinilab@gmail.com>
2025-04-25 16:26:44 -0500

    new

--- _functions/backend/[...catchall].js
+++ _functions/backend/[...catchall].js
@@ -1,15 +1,14 @@
 export async function onRequest({ request }) {
-    const url = new URL(request.url);
-    const proxyPath = url.pathname.replace("/backend", "/backend"); // se mantiene igual
-    const targetUrl = "https://backend-j5m6.onrender.com" + proxyPath;
+  const url = new URL(request.url);
+  const proxyPath = url.pathname.replace(/^\/backend/, "");
+  const targetUrl = "https://backend-j5m6.onrender.com" + proxyPath;
   
-    const proxyRequest = new Request(targetUrl, {
-      method: request.method,
-      headers: request.headers,
-      body: request.body,
-      redirect: "manual",
-    });
-  
-    return await fetch(proxyRequest);
-  }
-  
+
+  const proxyRequest = new Request(targetUrl, {
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
+    redirect: "manual",
+  });
+  return await fetch(proxyRequest);
+}
